a1b0e9bbb2f817f0b26bfe02bc49116a326be0e3
Sujin <ohs710@newschool.edu>
2018-11-13 19:57:43 -0500

    koalas
    koalification

---
+++ InclassJs/js/inclass.js
@@ -0,0 +1,8 @@
+var question= prompt("Why aren't Koalas actual bears?");
+
+
+if (question == "They do not meet the koalifications"){
+	alert ("Hey,How did you know?");
+}else{
+	alert("They don't meet the koalifications.");
+}
