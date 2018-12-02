2963c09611db4a54dcc9d85cc767faf4cf726340
Sujin <ohs710@newschool.edu>
2018-12-01 20:28:44 -0500

    wowmst
    wowmst

---
+++ loops/js/loops.js
@@ -0,0 +1,11 @@
+function blueCircle(diameter){
+	$('.wrapper').append("<div></div");
+	$('div').addClass("round");
+	$('div').css("width",diameter+ 'px');
+	$('div').css("height",diameter+'px'); 
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
