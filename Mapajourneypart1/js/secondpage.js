3b3abef35412cfa0f1af035b0e9657917f329c7e
Sujin <ohs710@newschool.edu>
2018-12-03 01:11:22 -0500

    oeaufhr
    arg

---
+++ Mapajourneypart1/js/secondpage.js
@@ -0,0 +1,18 @@
+$('#dorm').previewer({
+  trigger: 'hover',
+  type: 'image',
+  src: 'IMG_9094.jpg'
+});
+
+$('#dorm').previewer({
+  containerCSS: {
+    "border": "1px solid #999",
+    "background-color": "#FFEE88",
+    "border-radius": "5px",
+    "padding": "6px"
+  };
+}
+$('#dorm').previewer({
+  beforeShow: function(){},
+  onShow: function(){}
+});
