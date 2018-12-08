21463ecee37b453442a492bb3446e9ec417fa4d8
Sujin <ohs710@newschool.edu>
2018-12-08 14:24:03 -0500

    kjn
    kjn

---
+++ Mapajourneypart1/js/onmywaytotraderjoes.js
@@ -0,0 +1,10 @@
+ 
+
+
+
+ $( function(.switchClass ) {
+    $( "#button" ).on( "click", function() {
+      $( ".newClass" ).switchClass( "newClass", "anotherNewClass", 1000 );
+      $( ".anotherNewClass" ).switchClass( "anotherNewClass", "newClass", 1000 );
+    });
+  } );
