acd5c08c41336da4024cc71e0f8b8ea1eb874eab
Sujin <ohs710@newschool.edu>
2018-12-11 00:27:59 -0500

    wow
    shes a coder

---
+++ Mapajourneypart1/js/index.js
@@ -0,0 +1,37 @@
+// set up text to print, each item in array is new line
+var aText = new Array(
+"My Journey to Trader Joe's"
+);
+var iSpeed = 100; 
+var iIndex = 0;
+var iArrLength = aText[0].length; 
+var iScrollAt = 20; 
+ 
+var iTextPos = 0; 
+var sContents = ''; 
+var iRow; 
+ 
+function typewriter()
+{
+ sContents =  ' ';
+ iRow = Math.max(0, iIndex-iScrollAt);
+ var destination = document.getElementById("typedtext");
+ 
+ while ( iRow < iIndex ) {
+  sContents += aText[iRow++] + '<br />';
+ }
+ destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
+ if ( iTextPos++ == iArrLength ) {
+  iTextPos = 0;
+  iIndex++;
+  if ( iIndex != aText.length ) {
+   iArrLength = aText[iIndex].length;
+   setTimeout("typewriter()", 500);
+  }
+ } else {
+  setTimeout("typewriter()", iSpeed);
+ }
+}
+
+
+typewriter();
