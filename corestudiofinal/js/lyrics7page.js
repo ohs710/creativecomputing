267e28db7d3a04727cdf32f379ff6116994de288
Sujin <ohs710@newschool.edu>
2018-12-17 15:00:49 -0500

    erg
    reg

---
+++ corestudiofinal/js/lyrics7page.js
@@ -0,0 +1,36 @@
+var aText = new Array(
+"You found another one, but I am the better one"
+);
+var iSpeed = 100; 
+var iIndex = 0;
+var iArrLength = aText[0].length; 
+var iScrollAt = 20; 
+ 
+var iTextPos = 0; 
+var sContents = ''; 
+var iRow; 
+ 
+function typewriter()
+{
+ sContents =  ' ';
+ iRow = Math.max(0, iIndex-iScrollAt);
+ var destination = document.getElementById("typedtext");
+ 
+ while ( iRow < iIndex ) {
+  sContents += aText[iRow++] + '<br />';
+ }
+ destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
+ if ( iTextPos++ == iArrLength ) {
+  iTextPos = 0;
+  iIndex++;
+  if ( iIndex != aText.length ) {
+   iArrLength = aText[iIndex].length;
+   setTimeout("typewriter()", 500);
+  }
+ } else {
+  setTimeout("typewriter()", iSpeed);
+ }
+};
+
+
+typewriter();
