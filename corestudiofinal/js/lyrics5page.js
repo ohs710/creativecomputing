74ef3e7e644957185feddb24216858bcc4019bd1
Sujin <ohs710@newschool.edu>
2018-12-17 13:23:37 -0500

    f
    f

--- corestudiofinal/js/lyrics5page.js
+++ corestudiofinal/js/lyrics5page.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+jQuery(document).ready(function(){
    $('h1').mousemove(function(e){
      var rXP = (e.pageX - this.offsetLeft-$(this).width()/2);
      var rYP = (e.pageY - this.offsetTop-$(this).height()/2);
